f7bdd531050e1084484b552a8dcde897134d0276
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-06 11:57:46 -0700

    refactor api request to service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,10 @@ class SearchController < ApplicationController
   def index
     zip = params[:search].to_i
 
-    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,storeType,distance,city,phone&apiKey=#{ENV['BEST_BUY_KEY']}"
-
-    response = JSON.parse(response.body, symbolize_names: true)
-    # @stores = JSON.parse(response.body, symbolize_names: true)[:stores]
+    response = StoreFinderService.new.get_response(zip)
+    # response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,storeType,distance,city,phone&apiKey=#{ENV['BEST_BUY_KEY']}"
+    #
+    # response = JSON.parse(response.body, symbolize_names: true)
 
     @stores = response[:stores]
     @total = response[:total]
--- app/services/store_finder_service.rb
+++ app/services/store_finder_service.rb
@@ -1,4 +1,18 @@
 class StoreFinderService
+  attr_reader :conn
 
+  def initialize
+    @conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/stores') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+  def get_response(zip)
+    response = conn.get("/(area(#{zip},25))?format=json&show=longName,storeType,distance,city,phone&apiKey=#{ENV['BEST_BUY_KEY']}")
+binding.pry
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
 end
