415cbd0362e78fd05d6f98c646e46472dd189699
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-06 11:43:53 -0700

    continue working on api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,13 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,storeType,distance,city,phone&apiKey="
-# binding.pry
-    @stores = JSON.parse(response.body, symbolize_names: true)[:stores]
+    zip = params[:search].to_i
+
+    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,storeType,distance,city,phone,pageSize=100&apiKey=#{ENV['BEST_BUY_KEY']}"
+
+    response = JSON.parse(response.body, symbolize_names: true)
+    @stores = response[:stores]
+    @total = response[:total]
 
   end
 
